5dc25d5594cc1b477b8cebc29b8aee171705821c
Rex Phung <khoatuanphungnguyen@gmail.com>
2022-08-30 15:37:57 -0700

    Add FourSquare API for Coffee Locations and Upsplash API for Pictures

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -5,12 +5,15 @@ import Link from 'next/link'
 import Image from 'next/image';
 import styles from '../../styles/coffee-store.module.css'
 import cls from "classnames"
+import { fetchCoffeeStores } from "../../lib/coffee-stores";
 
 export async function getStaticProps(staticProps) {
   const params = staticProps.params;
+  const coffeeStores = await fetchCoffeeStores();
+
   return {
     props: {
-      coffeeStore: coffeeStoresData.find((coffeeStore) => {
+      coffeeStore: coffeeStores.find((coffeeStore) => {
         return coffeeStore.id.toString() === params.id; //dynamic id
       })
     },
@@ -18,8 +21,10 @@ export async function getStaticProps(staticProps) {
 }
 
 
-export function getStaticPaths() {
-    const paths = coffeeStoresData.map((coffeeStore)=>{
+export  async function getStaticPaths() {
+  const coffeeStores = await fetchCoffeeStores();
+
+  const paths = coffeeStores.map((coffeeStore)=>{
       return {
         params: {
           id: coffeeStore.id.toString(),
@@ -35,7 +40,7 @@ return {
 const DynamicRoute = ((props) => {
   const router = useRouter();
   const query = router.query.id;
-  const {imgUrl, address,name,neighbourhood} = props.coffeeStore
+  const {name,address,neighborhood,imgUrl} = props.coffeeStore
 
   const handleUpvoteButton = () => {
     console.log("handle upvote")
@@ -53,28 +58,31 @@ const DynamicRoute = ((props) => {
       </div>
       <div className={styles.container}>
       <div className ={styles.col1}>
-      <Link href="/">Back to Home</Link>
+      <Link href="/">← Back to Home</Link>
       <div className ={styles.nameWrapper}>  
 
       <h1 className ={styles.name}>{name}</h1>
 
       </div>
     
-      <Image src={imgUrl} width={600} height={360} className={styles.storeImg} alt={name} />
+      <Image src={imgUrl ||"https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80"} width={600} height={360} className={styles.storeImg} alt={name} />
       </div>
 
       <div className ={cls("glass",styles.col12)}>
-
-      <div className ={styles.iconWrapper}>
+      { address && ( <div className ={styles.iconWrapper}>
       <Image src={"/static/icons/storefront.svg"} width={24} height={24}/>
       <p className ={styles.text}>{address}</p>
       </div>
-
-      <div className ={styles.iconWrapper}>
+      
+      )}
+     
+      {neighborhood && (
+        <div className ={styles.iconWrapper}>
       <Image src={"/static/icons/nearMe.svg"} width={24} height={24}/>
-      <p className ={styles.text}>{neighbourhood}</p>
+      <p className ={styles.text}>{neighborhood}</p>
       </div>
-
+      )}
+     
       <div className ={styles.iconWrapper}>
       <Image src={"/static/icons/stars.svg"} width={24} height={24}/>
       <p className ={styles.text}>{1}</p>
